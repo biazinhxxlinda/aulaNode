801bf230222c08b79fca043457f9fa42573d8698
kitkeith <cauef76@gmail.com>
2022-11-23 10:01:08 -0300

    versao 2

--- teste.js
+++ teste.js
@@ -1,13 +1,14 @@
 
 var http = require('http');
 var n = require ('./primeiromodulo');
+var dia = require('./moduloData');
 http.createServer(function (req, res){
     res.writeHead(200,{'content-type':'text/html'});
     res.write('<b>Aula de node.js<b> <br>');
     res.write('<i>Aluna: </i>');
     res.write(n.nome()+'<br>');
-    res.write(Date());
-
+    //res.write(Date());
+    res.write(dia.diaDaSemana()+'<br>');
     res.end('bom dia');
 
 }).listen(80);
